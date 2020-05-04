f9a21be4c0f7b0782a6b9d05156d39804434060e
dracarys18 <karthihegde010@gmail.com>
2020-05-05 01:26:34 +0530

    vince: prop: enable framework boost
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -149,6 +149,17 @@ ro.fm.transmitter=false \
 vendor.hw.fm.init=0 \
 ro.vendor.fm.use_audio_session=true
 
+# Framework boost
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.at_library=true \
+vendor.enable.prefetch=false \
+vendor.iop.enable_prefetch_ofr=false \
+vendor.iop.enable_uxe=1 \
+vendor.perf.gestureflingboost.enable=true \
+vendor.perf.iop_v3.enable.debug=false \
+vendor.perf.iop_v3.enable=true \
+vendor.perf.workloadclassifier.enable=true
+
 # Frp
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
