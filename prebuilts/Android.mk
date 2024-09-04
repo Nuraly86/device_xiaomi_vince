nekoprjkt/device-xiaomi-vince

vince: prebuilts: Add GalleryGo
Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

prebuilts/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GalleryGo
+LOCAL_SRC_FILES := priv-app/GalleryGo/GalleryGo.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Photos Gallery2 SnapdragonGallery
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)

Nuraly86/device_xiaomi_vince

Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

prebuilts/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GalleryGo
+LOCAL_SRC_FILES := priv-app/GalleryGo/GalleryGo.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Photos Gallery2 SnapdragonGallery
+LOCAL_CERTIFICATE := platform
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)