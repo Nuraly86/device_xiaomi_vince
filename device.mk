14f2d8b90e29b1bf36347da4b272b326a5824422
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2024-12-27 07:30:35 +0000

    fixUp "vince: Switch to new AIDL IR service"
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -125,8 +125,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Display
 PRODUCT_PACKAGES += \
